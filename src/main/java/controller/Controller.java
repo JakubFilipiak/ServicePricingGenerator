bf84aebcd72c61fe5949965666798919f3f83ab6
Jakub <kuba.filipiak52@gmail.com>
2019-02-01 07:14:00 +0100

    Added a simple .fxml file and created a simple project structure.

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,8 @@
+package controller;
+
+/**
+ * Created by Jakub Filipiak on 31.01.2019.
+ */
+public class Controller {
+
+}
---
+++ src/main/java/view/App.java
@@ -0,0 +1,28 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by Jakub Filipiak on 31.01.2019.
+ */
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(
+                "/MainView.fxml"));
+        Parent layout = fxmlLoader.load();
+
+        Scene scene = new Scene(layout);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Service Pricing Generator");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
